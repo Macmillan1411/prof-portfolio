1ce57da94aad30e25ae00b51ea5f272068f613e5
Macmillan1411 <macmillantapiwanashe1411>
2024-03-30 13:53:56 +0300

    Allowed hosts changes to python anywhere

--- portfolioproject/settings.py
+++ portfolioproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(8k0y(%-0l4pyar7skd%!%3#yi7(u@jjb070bsuh1_$_l#3m7@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['macmillan.pythonanywhere.com','localhost','127.0.0.1']
 
 
 # Application definition
